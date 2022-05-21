e09b0eb0e0b4b90f8816b384f3c0bfd46a610b00
lucasmonsan <lucasmonsan@gmail.com>
2022-05-21 18:48:59 -0300

    Atualizado

---
+++ aula-variaveis/index.js
@@ -0,0 +1,18 @@
+prompt("Digite qualquer coisa")
+
+firstName = ""
+lastName = ""
+age = 0
+group = ""
+
+firstName = prompt("Qual o seu primeiro nome?")
+lastName = prompt("Qual o seu sobrenome?")
+age = prompt("Qual a sua idade?")
+group = prompt("Qual a sua turma?")
+
+console.log("Seu nome completo é",firstName,lastName,",você tem",age,"anos e faz parte da turma",group,"!")
+console.log("-----------------------")
+
+console.log("O tipo da variável 'age' é:",typeof age)
+console.log("-----------------------")
+
