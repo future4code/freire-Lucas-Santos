91fbc470453a1241cba9bf452fd715b02a86e151
lucasmonsan <lucasmonsan@gmail.com>
2022-05-27 16:53:03 -0300

    update dos arquivos

---
+++ modulo1/pratica_ingles_erros_pesquisa/index.js
@@ -0,0 +1,32 @@
+const myFunction = () => {
+    const variavel = 9;
+    return 7;
+}
+
+
+const fazLog = () => {
+    console.log(evento);
+}
+
+fazLog()
+
+const subtrai = (el1, el2) => el1 - el2;
+subtrair(2,3)
+
+const minhaFuncao = () => {
+
+}
+
+const minhaArray = [
+    {title: "nome 1"},
+    {title: "nome 2"},
+    {title: "nome 3"},
+    {title: "nome 4"}
+];
+const minhaArrayTransformada = minhaArray.map((element, index) => {
+    if(index > 0) {
+    return element;
+    }
+});
+
+console.log(minhaArrayTransformada[0].title);
