622c4c2ef52f49227c696cbf8a1221f89062a96e
lucasmonsan <lucasmonsan@gmail.com>
2022-07-12 14:35:49 -0300

    Background do site

--- modulo3/Freire-labex/src/App.jsx
+++ modulo3/Freire-labex/src/App.jsx
@@ -1,4 +1,4 @@
-import "./styles/Reset_Default.css";
+import "./styles/App.css";
 
 //Pages
 import { HomePage } from './pages/HomePage.jsx';
@@ -6,7 +6,13 @@ import { HomePage } from './pages/HomePage.jsx';
 function App() {
   return (
     <div className="App">
-      <HomePage />
+      <div className="Background-box">
+        <video 
+          autoPlay loop 
+          src="https://firebasestorage.googleapis.com/v0/b/monsan-dev.appspot.com/o/LabeX%2Fbackground.webm?alt=media&token=f0348065-3d40-4521-8bc6-696dd5c14e07" 
+          type="video/webm" 
+        />
+      </div>      
     </div>
   )
 }
--- modulo3/Freire-labex/src/components/Header.jsx
+++ modulo3/Freire-labex/src/components/Header.jsx
@@ -1,17 +1,12 @@
 import "../styles/Reset_Default.css";
 
 //Images
-import logo from "../icons/logo.svg";
-
-//Stitches Tags
-import { Img, Strong, Span } from "../styles/Comps.jsx";
+import logo from "../images/logo.svg";
 
 export const Header = () => {
   return (
-    <div className="Header">
-      <Img type="logo" src={logo} alt="logo" />
-      <Strong size="md" type="link">Quem somos?</Strong>
-      <Strong size="md" type="link">Olá, viajante!</Strong>
+    <div>
+
     </div>
   )
 }
