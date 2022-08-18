36c388555bd9d450ed6bfd63a2d0cb06f48b9466
lucasmonsan <lucasmonsan@gmail.com>
2022-08-18 10:19:03 -0300

    Pra apagar as alterações anteriores

--- modulo4/node-package-json/aula/index.js
+++
@@ -1,9 +0,0 @@
-import { countries } from "./countries.js";
-
-const myCountrie = process.argv[2]
-
-const result = countries.filter(countrie => {
-  return countrie.name.toLowerCase().includes(myCountrie)
-})
-
-console.table(result)
